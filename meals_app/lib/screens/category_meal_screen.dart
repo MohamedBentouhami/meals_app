40d6f649d62e2926b98878bb40fc664635f9c6c0
Mohamed BENTOUHAMI <mbentouhami@adflux.net>
2023-02-13 17:30:06 +0100

    add meal screen

---
+++ meals_app/lib/screens/category_meal_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import '../dummy_data.dart';
+import '../widgets/meal_item.dart';
+
+class CategoryMealScreen extends StatelessWidget {
+  static const routeName = '/category_meals';
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context)?.settings.arguments as Map<String, String>;
+    final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(categoryTitle.toString()),
+        ),
+        body: ListView.builder(
+          itemBuilder: (context, index) {
+            return MealItem(
+              title: categoryMeals[index].title,
+              affordability: categoryMeals[index].affordability,
+              complexity: categoryMeals[index].complexity,
+              duration: categoryMeals[index].duration,
+              imageUrl: categoryMeals[index].imageUrl,
+            );
+          },
+          itemCount: categoryMeals.length,
+        ));
+  }
+}
