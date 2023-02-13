257e4d28e31a1f282bf0a963cf2981ac8f76634c
Mohamed BENTOUHAMI <mbentouhami@adflux.net>
2023-02-13 14:27:22 +0100

    end catgeries screen

--- meals_app/lib/categories_screen.dart
+++ meals_app/lib/categories_screen.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/category_item.dart';
+
+import './dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: <Widget>[],
-      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+    return Scaffold(
+      appBar: AppBar(title: const Text('Deli Meals')),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
           childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20),
+          mainAxisSpacing: 20,
+          crossAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES.map((catData) {
+          return CategoryItem(catData.title, catData.color);
+        }).toList(),
+      ),
     );
   }
 }
--- meals_app/lib/main.dart
+++ meals_app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './categories_screen.dart';
+
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -10,29 +12,24 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+            bodySmall: const TextStyle(
+              color: Color.fromRGBO(20, 51, 51, 1),
+            ),
+            bodyMedium: const TextStyle(
+              color: Color.fromRGBO(20, 51, 51, 1),
+            ),
+            titleMedium: const TextStyle(
+                fontFamily: 'RobotoCondensed',
+                fontSize: 20,
+                fontWeight: FontWeight.bold)),
       ),
-      home: MyHomePage(),
+      home: CategoriesScreen(),
       debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('DeliMeals'),
-      ),
-      body: const Center(
-        child: Text('Navigation Time!'),
-      ),
     );
   }
 }
